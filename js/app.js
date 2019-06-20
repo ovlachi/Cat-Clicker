402d205b741c6e8824f8f3f8807297c2be6a990a
George <g.ovlachi@gmail.com>
2019-06-21 08:08:00 +1000

    javascript added

---
+++ js/app.js
@@ -0,0 +1,7 @@
+
+document.getElementById ("clickme").addEventListener ("click", updateCounter, false);
+const count = 0;
+    function updateCounter() {
+        count++;  
+        document.getElementById("counter").innerHTML = "Counter: " + count;
+    }
